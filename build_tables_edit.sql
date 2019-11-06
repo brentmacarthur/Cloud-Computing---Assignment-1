161117b97b46a65bc3c6db39ca9d5a3a15a99b4f
Brent MacArthur <bmacarthur67@gmail.com>
2019-11-05 22:20:37 -0800

    Reformat and change "Order" table to Orders

--- build_tables_edit.sql
+++ build_tables_edit.sql
@@ -26,144 +26,111 @@ create index IndexCustomerName on Customer (
 /*==============================================================*/
 /* Table: "Order"                                               */
 /*==============================================================*/
-create table "Order"
+create table Orders
 (
     Id          int             auto_increment,
-    OrderDate   datetime        not null default getdate(),
+    OrderDate   DATETIME        not null 	default CURRENT_TIMESTAMP(),
     OrderNumber nvarchar(10)    null,
     CustomerId  int             not null,
     TotalAmount decimal(12,2)   null default 0,
     constraint PK_ORDER primary key (Id)
 );
 
-        /*==============================================================*/
-        /* Index: IndexOrderCustomerId                                  */
-        /*==============================================================*/
-        create index IndexOrderCustomerId on "Order" (
-CustomerId ASC
+/*==============================================================*/
+/* Index: IndexOrderCustomerId                                  */
+/*==============================================================*/
+create index IndexOrderCustomerId on Orders (CustomerId ASC);
+
+/*==============================================================*/
+/* Index: IndexOrderOrderDate                                   */
+/*==============================================================*/
+create index IndexOrderOrderDate on Orders (OrderDate ASC);
+
+/*==============================================================*/
+/* Table: OrderItem                                             */
+/*==============================================================*/
+create table OrderItem
+(
+    Id          int             auto_increment,
+    OrderId     int             not null,
+    ProductId   int             not null,
+    UnitPrice   decimal(12,2)   not null    default 0,
+    Quantity    int             not null    default 1,
+    constraint PK_ORDERITEM primary key (Id)
 );
 
-        /*==============================================================*/
-        /* Index: IndexOrderOrderDate                                   */
-        /*==============================================================*/
-        create index IndexOrderOrderDate on "Order" (
-OrderDate ASC
+/*==============================================================*/
+/* Index: IndexOrderItemOrderId                                 */
+/*==============================================================*/
+create index IndexOrderItemOrderId on OrderItem (OrderId ASC);
+
+/*==============================================================*/
+/* Index: IndexOrderItemProductId                               */
+/*==============================================================*/
+create index IndexOrderItemProductId on OrderItem (ProductId ASC);
+
+/*==============================================================*/
+/* Table: Product                                               */
+/*==============================================================*/
+create table Product
+(
+    Id          int             auto_increment,
+    ProductName nvarchar(50)    not null,
+    SupplierId  int             not null,
+    UnitPrice   decimal(12,2)   null    default 0,
+    Package     nvarchar(30)    null,
+    IsDiscontinued bit          not null default 0,
+    constraint PK_PRODUCT primary key (Id)
 );
 
-        /*==============================================================*/
-        /* Table: OrderItem                                             */
-        /*==============================================================*/
-        create table OrderItem
-        (
-            Id int
-            auto_increment,
-    OrderId int not null,
-    ProductId int not null,
-    UnitPrice decimal
-            (12,2) not null default 0,
-    Quantity int not null default 1,
-    constraint PK_ORDERITEM primary key
-            (Id)
+/*==============================================================*/
+/* Index: IndexProductSupplierId                                */
+/*==============================================================*/
+create index IndexProductSupplierId on Product (SupplierId ASC);
+
+/*==============================================================*/
+/* Index: IndexProductName                                      */
+/*==============================================================*/
+create index IndexProductName on Product (ProductName ASC);
+
+/*==============================================================*/
+/* Table: Supplier                                              */
+/*==============================================================*/
+create table Supplier
+(
+    Id          int             auto_increment,
+    CompanyName nvarchar(40)    not null,
+    ContactName nvarchar(50)    null,
+    ContactTitle nvarchar(40)   null,
+    City        nvarchar(40)    null,
+    Country     nvarchar(40)    null,
+    Phone       nvarchar(30)    null,
+    Fax         nvarchar(30)    null,
+    constraint PK_SUPPLIER primary key (Id)
 );
 
-            /*==============================================================*/
-            /* Index: IndexOrderItemOrderId                                 */
-            /*==============================================================*/
-            create index IndexOrderItemOrderId on OrderItem (
-OrderId ASC
-);
+/*==============================================================*/
+/* Index: IndexSupplierName                                     */
+/*==============================================================*/
+create index IndexSupplierName on Supplier (CompanyName ASC);
 
-            /*==============================================================*/
-            /* Index: IndexOrderItemProductId                               */
-            /*==============================================================*/
-            create index IndexOrderItemProductId on OrderItem (
-ProductId ASC
-);
+/*==============================================================*/
+/* Index: IndexSupplierCountry                                  */
+/*==============================================================*/
+create index IndexSupplierCountry on Supplier (Country ASC);
 
-            /*==============================================================*/
-            /* Table: Product                                               */
-            /*==============================================================*/
-            create table Product
-            (
-                Id int
-                auto_increment,
-    ProductName nvarchar
-                (50) not null,
-    SupplierId int not null,
-    UnitPrice decimal
-                (12,2) null default 0,
-    Package nvarchar
-                (30) null,
-    IsDiscontinued bit not null default 0,
-    constraint PK_PRODUCT primary key
-                (Id)
-);
-
-                /*==============================================================*/
-                /* Index: IndexProductSupplierId                                */
-                /*==============================================================*/
-                create index IndexProductSupplierId on Product (
-SupplierId ASC
-);
-
-                /*==============================================================*/
-                /* Index: IndexProductName                                      */
-                /*==============================================================*/
-                create index IndexProductName on Product (
-ProductName ASC
-);
-
-                /*==============================================================*/
-                /* Table: Supplier                                              */
-                /*==============================================================*/
-                create table Supplier
-                (
-                    Id int
-                    auto_increment,
-    CompanyName nvarchar
-                    (40) not null,
-    ContactName nvarchar
-                    (50) null,
-    ContactTitle nvarchar
-                    (40) null,
-    City nvarchar
-                    (40) null,
-    Country nvarchar
-                    (40) null,
-    Phone nvarchar
-                    (30) null,
-    Fax nvarchar
-                    (30) null,
-    constraint PK_SUPPLIER primary key
-                    (Id)
-);
-
-                    /*==============================================================*/
-                    /* Index: IndexSupplierName                                     */
-                    /*==============================================================*/
-                    create index IndexSupplierName on Supplier (
-CompanyName ASC
-);
-
-                    /*==============================================================*/
-                    /* Index: IndexSupplierCountry                                  */
-                    /*==============================================================*/
-                    create index IndexSupplierCountry on Supplier (
-Country ASC
-);
-
-                    alter table "Order"
+alter table Orders
    add constraint FK_ORDER_REFERENCE_CUSTOMER foreign key (CustomerId)
       references Customer (Id);
 
-                    alter table OrderItem
+alter table OrderItem
    add constraint FK_ORDERITE_REFERENCE_ORDER foreign key (OrderId)
-      references "Order" (Id);
+      references Orders (Id);
 
-                    alter table OrderItem
+alter table OrderItem
    add constraint FK_ORDERITE_REFERENCE_PRODUCT foreign key (ProductId)
       references Product (Id);
 
-                    alter table Product
+alter table Product
    add constraint FK_PRODUCT_REFERENCE_SUPPLIER foreign key (SupplierId)
       references Supplier (Id);
